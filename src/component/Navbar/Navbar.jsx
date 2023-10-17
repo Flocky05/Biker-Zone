dffe22b7ee19b946d0b5d53afe672849a89eac99
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-17 16:09:01 +0600

    update navbar section

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
-    return (
+  return (
+    <div className="flex justify-between p-5">
         <div>
-            <h2>This is Navbar section</h2>
+          <Link to="/" className="text-3xl font-bold">Biker Zone</Link>
         </div>
-    );
+        <div className="grid grid-cols-6 gap-4 items-center text-xl">
+              <Link to="/">Home</Link>
+              <Link to="/">Shop</Link>
+              <Link to="/">News</Link>
+              <Link to="/">Contract</Link>
+              <Link to="/" className="bg-orange-600 text-white px-6 py-2 rounded-lg">Login</Link>
+              
+        </div>
+    </div>
+  );
 };
 
 export default Navbar;
