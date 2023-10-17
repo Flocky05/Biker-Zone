04bbb9bcaa755f0b35abd43873582da890b0fe12
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-17 12:53:22 +0600

    added carousel section

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import Main from './component/Main/Main';
 import Home from './component/Home/Home';
+import { ThemeProvider } from "@material-tailwind/react";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,8 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
