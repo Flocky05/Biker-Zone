0a1f890138d4523fc61e66a669305716c4f64353
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-17 23:56:41 +0600

    update letest Bike section

--- src/component/Letest Bike/LetestBike.jsx
+++ src/component/Letest Bike/LetestBike.jsx
@@ -6,29 +6,29 @@ const LetestBike = () => {
     return (
         <div>
             <h2 className="text-5xl text-center p-14">Latest Bikes</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 m-20">
-                <div className="bg-orange-100 p-10 rounded-lg">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 m-8 md:m-12 lg:m-20">
+                <div className="bg-orange-100 p-8 lg:p-10 rounded-lg">
                     <img src={img1} alt="" />
                     <div className="mt-3">
                         <h2>Ducati XDIAVEL S-73</h2>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est illum veritatis quam magnam!</p>
-                        <button className="bg-orange-900 text-white px-6 py-2 rounded-md">Purchase</button>
+                        <button className="bg-orange-900 text-white px-6 py-2 rounded-md mt-3">Purchase</button>
                     </div>
                 </div>
-                <div className="bg-orange-100 p-10  rounded-lg">
+                <div className="bg-orange-100 p-8 lg:p-10  rounded-lg">
                     <img src={img2} alt="" />
                     <div className="mt-3">
                         <h2>Ducati XDIAVEL S-73</h2>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est illum veritatis quam magnam!</p>
-                        <button className="bg-orange-900 text-white px-6 py-2 rounded-md">Purchase</button>
+                        <button className="bg-orange-900 text-white px-6 py-2 rounded-md mt-3">Purchase</button>
                     </div>
                 </div>
-                <div className="bg-orange-100 p-10  rounded-lg">
+                <div className="bg-orange-100 p-8 lg:p-10  rounded-lg">
                     <img src={img3} alt="" />
                     <div className="mt-3">
                         <h2>Ducati XDIAVEL S-73</h2>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est illum veritatis quam magnam!</p>
-                        <button className="bg-orange-900 text-white px-6 py-2 rounded-md">Purchase</button>
+                        <button className="bg-orange-900 text-white px-6 py-2 rounded-md mt-3">Purchase</button>
                     </div>
                 </div>
             </div>
