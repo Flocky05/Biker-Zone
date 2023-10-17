e48fe34e0a6f320f4376ac1fe2a82660fff28f5a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-17 16:34:03 +0600

    make responsive carousel

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,24 +1,46 @@
 import { Carousel } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
         <div>
-           <Carousel className="rounded-xl">
-      <img
-        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-        alt="image 1"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-        alt="image 2"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-        alt="image 3"
-        className="h-full w-full object-cover"
-      />
+           <Carousel>
+      <div className="relative">
+        <img
+            src="https://images.unsplash.com/photo-1648204894242-ca89818edc26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            alt="image 2"
+            className="h-full w-full object-cover"
+        />
+        <div className="absolute -mt-32 lg:-mt-96 px-20 z-10">
+                <h2 className="text-white text-2xl lg:text-4xl text-center">Ride-on R15 V4 with Smile</h2>
+                <p className="text-white text-sm lg:text-lg text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum nobis numquam voluptatum dolorem error officia, aperiam quae iste  soluta maxime suscipit est consequuntur?</p>
+
+            </div>
+      </div>
+      <div className="relative">
+            <img
+                src="https://images.unsplash.com/photo-1580451999426-26891208ae4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                alt="image 2"
+                className="h-full w-full object-cover"
+            />
+            <div className="absolute -mt-32 lg:-mt-96 px-20 z-10">
+                <h2 className="text-white text-2xl lg:text-4xl text-center">Ride-on R15 V4 with Smile</h2>
+                <p className="text-white text-sm lg:text-lg text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum nobis numquam voluptatum dolorem error officia, aperiam quae iste voluptates  soluta maxime suscipit est consequuntur?</p>
+
+            </div>
+      </div>
+      <div className="relative">
+            <img
+                src="https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                alt="image 3"
+                className="h-full w-full object-cover"
+            />
+            <div className="absolute -mt-32 lg:-mt-96 px-20 z-10">
+                <h2 className="text-white text-2xl lg:text-4xl text-center">Ride-on R15 V4 with Smile</h2>
+                <p className="text-white text-sm lg:text-lg text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum nobis numquam voluptatum dolorem error officia, aperiam quae iste voluptates  maxime suscipit est consequuntur?</p>
+
+            </div>
+      </div>
     </Carousel>
         </div>
     );
