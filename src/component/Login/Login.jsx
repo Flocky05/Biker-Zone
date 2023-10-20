91d1240d16a11d7e897d76b42f0f991844f3a5de
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-20 23:53:16 +0600

    register section added

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
-        <div className="bg-purple-400 grid grid-cols-1 justify-items-center p-20">
+        <div className="bg-purple-300 grid grid-cols-1 justify-items-center p-20">
+            <h2 className="text-3xl p-12 text-black hover:text-white">Please <span className="text-white hover:text-black">Login</span> </h2>
             <form action="">
-                <h2>Enter your Mail</h2>
+                <h2 className="text-lg">Enter your Mail</h2>
                 <input type="mail" className="p-2 border-2 w-full bg-blue-gray-50 rounded-md" placeholder="Enter your mail"/>
-                <h2>Enter your password</h2>
+                <h2 className="text-lg">Enter your password</h2>
                 <input type="password" className="p-2 w-full border-2 bg-blue-gray-50 rounded-md" placeholder="Enter your password"/>
+                <p className="mt-3">Do not have <Link to="/register" className="text-white">Accout</Link>? </p>
             </form>
         </div>
     );
---
+++ src/component/Register/Register.jsx
@@ -0,0 +1,11 @@
+
+
+const Register = () => {
+    return (
+        <div>
+            <h2>This is Register section</h2>
+        </div>
+    );
+};
+
+export default Register;
