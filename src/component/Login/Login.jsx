82d78000fdd6eb4b073983ffc1b610b41c282f3c
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-20 23:43:41 +0600

    update login page

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -2,8 +2,13 @@
 
 const Login = () => {
     return (
-        <div>
-            <h2>this is login page</h2>
+        <div className="bg-purple-400 grid grid-cols-1 justify-items-center p-20">
+            <form action="">
+                <h2>Enter your Mail</h2>
+                <input type="mail" className="p-2 border-2 w-full bg-blue-gray-50 rounded-md" placeholder="Enter your mail"/>
+                <h2>Enter your password</h2>
+                <input type="password" className="p-2 w-full border-2 bg-blue-gray-50 rounded-md" placeholder="Enter your password"/>
+            </form>
         </div>
     );
 };
